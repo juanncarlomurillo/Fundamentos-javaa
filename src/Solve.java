fb93cc03342eaf5b4a5026bd76c010ec38a72080
Juan Carlos Murillo Quintero <juancarlosmurilloquintero13@gmail.com>
2024-07-29 07:19:35 -0500

    third commit

---
+++ src/Solve.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Solve {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Menú:");
+        System.out.println("1. Ejercicio 1");
+        System.out.println("2. Ejercicio 2");
+        System.out.println("3. Ejercicio 3");
+        System.out.println("4. Ejercicio 4");
+        System.out.println("5. Ejercicio 5");
+        System.out.println("6. Ejercicio 6");
+        System.out.println("7. Ejercicio 7");
+        System.out.println("8. Ejercicio 8");
+        System.out.println("9. Ejercicio 9");
+        System.out.println("10. Ejercicio 10");
+        System.out.println("11. Salir");
+        System.out.print("Ingrese una opción: ");
+        int opcion = scanner.nextInt();
+    }
+}
+
+
